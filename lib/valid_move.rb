00921e5dcd0f53ea089cb85ca4b0a5ce8a51b408
S. Jones <jonesitis7@gmail.com>
2018-08-03 15:13:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board,index)
   if !position_taken?(board,place) && place.between?(0,8)
     return true
 elsif position_taken(board,place) && place.between(0,8)
-  return true
+  return false
 else
   return false
   end
