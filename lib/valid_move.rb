5f8d9513028a5c5bec99e29099b637f433ecef5f
S. Jones <jonesitis7@gmail.com>
2018-08-03 15:00:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,25 +1,7 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-# code your input_to_index and move method here!
-def input_to_index(user_input)
-  number_entered = user_input.to_i - 1
-  return number_entered
-end
-
-def move(board, index, character = "X")
-  board[index] = character
-  return board
-end
-
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if position_taken?(board,index)
+  place = position.to_i - 1
+  if !position_taken?(board,index) && place.between?(0,8)
     return true
 else
   return false
